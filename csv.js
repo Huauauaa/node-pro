ff50d9fa1354d2d71211f123b6f09a5a5f8d272d
Harvey <harvey0379@163.com>
2019-10-02 11:28:58 +0800

    filter excel

---
+++ csv.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+exports.writeCSV = (filename, content) => {
+  console.log(filename, content)
+  fs.writeFile(filename, content, err => {
+    if (err) {
+      console.error('Server is error...')
+    }
+  })
+}
