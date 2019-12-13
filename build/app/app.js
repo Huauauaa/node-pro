67af3f8726939c512eddde370ed2e380c209210b
Harvey <harvey0379@163.com>
2019-12-13 14:06:21 +0800

    feat:api proxy

--- build/app/app.js
+++ build/app/app.js
@@ -5,8 +5,25 @@ var app = express();
 app.get('/users', function (request, response) {
     response.send([
         {
-            id: 1
-        }
+            key: '1',
+            name: 'Mike',
+            id: 32,
+            picture: {
+                "large": "https://randomuser.me/api/portraits/men/75.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+            },
+        },
+        {
+            key: '2',
+            name: 'John',
+            id: 42,
+            picture: {
+                "large": "https://randomuser.me/api/portraits/men/75.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+            },
+        },
     ]);
 });
 app.listen(3001, function () {
