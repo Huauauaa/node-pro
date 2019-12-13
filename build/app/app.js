a035a5796a10da63e7c9ed55b1be899a98c58735
Harvey <harvey0379@163.com>
2019-12-13 11:23:32 +0800

    chore: vsCode debug

--- build/app/app.js
+++ build/app/app.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 app.get('/', function (request, response) {
+    debugger
+    console.log(1);
     response.send('Hello world.');
+    console.log(2);
 });
 app.listen(3000, function () {
     console.log('app is running on port 3000');
