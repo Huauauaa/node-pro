fd019c51be6962867a63496a1aeb7a007ba1c10d
Harvey <harvey0379@163.com>
2020-03-09 08:42:14 +0800

    lint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import express = require('express');
+// import express = require('express');
+import express from 'express';
 
 import todo from './data/todo.json';
 import tags from './data/tags.json';
@@ -8,30 +9,28 @@ import formulas1 from './data/formulas1.json';
 const app: express.Application = express();
 
 app.get('/users', (request, response) => {
-  response.send(
-    [
-      {
-        key: '1',
-        name: 'Mike',
-        id: 32,
-        picture: {
-          "large": "https://randomuser.me/api/portraits/men/75.jpg",
-          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-      },
-      {
-        key: '2',
-        name: 'John',
-        id: 42,
-        picture: {
-          "large": "https://randomuser.me/api/portraits/men/75.jpg",
-          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-      },
-    ]
-  );
+  response.send([
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/75.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/75.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/75.jpg'
+      }
+    },
+    {
+      key: '2',
+      name: 'John',
+      id: 42,
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/75.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/75.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/75.jpg'
+      }
+    }
+  ]);
 });
 
 app.get('/intblfml/v1/plugins/intblfml/todo', (request, response) => {
@@ -42,22 +41,27 @@ app.get('/intblfml/v1/tags', (request, response) => {
   response.send(tags);
 });
 
-app.get('/intblfml/v1/plugins/intblfml/documents/2964/question/372303/intable-formulas', (request, response) => {
-  response.send(formulas);
-});
+app.get(
+  '/intblfml/v1/plugins/intblfml/documents/2964/question/372303/intable-formulas',
+  (request, response) => {
+    response.send(formulas);
+  }
+);
 
-app.get('/intblfml/v1/plugins/intblfml/documents/2964/question/372242/intable-formulas', (request, response) => {
-  response.send(formulas1);
-});
+app.get(
+  '/intblfml/v1/plugins/intblfml/documents/2964/question/372242/intable-formulas',
+  (request, response) => {
+    response.send(formulas1);
+  }
+);
 
 app.post('/intblfml/v1/question/372303/answer', (request, response) => {
   response.send({});
-})
+});
 
 app.post('/intblfml/v1/question/372242/answer', (request, response) => {
   response.send({});
-})
-
+});
 
 app.listen(3001, () => {
   console.log('app is running');
